31d9c2d5256b28d5dae9913f68864f5ae9294975
Olcay DEMIRCAN <lloycca@gmail.com>
2021-01-16 23:42:49 +0100

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Programlamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım Geliştirici Yetiştirme Kampı", "Programlamaya başlangıç için temel kurs", "Java", "Phyton" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("for bitti");
+
+            foreach (var item in collection)
+            {
+
+            }
+
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+
+            }
+
+            Console.WriteLine("sayfa sonu");
+        }
+    }
+}
